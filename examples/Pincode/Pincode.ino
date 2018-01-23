b980a99cb098a73398c3b7dd5d6797cd398271f7
marplaa <marplaa@users.noreply.github.com>
2018-01-23 19:53:39 +0100

    Update Pincode.ino

--- examples/Pincode/Pincode.ino
+++ examples/Pincode/Pincode.ino
@@ -6,51 +6,44 @@
 #define OLED_CS     2
 #define OLED_RESET  15
 
+#define BUTTON      4
+
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 Encoder myEnc(20, 21);
+Pincode pincode(&display, &myEnc, BUTTON);
 
-byte pin[] = {1,2,3,4};
+byte pin[] = {1, 2, 3, 4};
 
 void setup() {
 
-	display.begin(SSD1306_SWITCHCAPVCC);
-
-	//pinMode(BUTTON, INPUT_PULLUP);           // set pin to input
-
-	display.setTextColor(WHITE);
-	display.setTextSize(1);
-	display.setTextWrap(false);
-	display.setFont(&FreeSans24pt7b);
+  display.begin(SSD1306_SWITCHCAPVCC);
+  pinMode(BUTTON, INPUT_PULLUP);           // set pin to input
+  pincode.setDialSpeed(20);
 }
 
 void loop() {
+  byte inputPin[4];
+  boolean ok = true;
+  pincode.setRandomSeed(analogRead(A0));
 
-	boolean ok = true;
+  pincode.reqPin(inputPin);
 
-	Pincode pincode(&display, &myEnc, 4);
-	pincode.setRandomSeed(analogRead(A0));
-	pincode.setDialSpeed(10);
+  for (int i = 0; i < 4; i++) {
+    if (inputPin[i] != pin[i]) {
+      ok = false;
+      break;
+    }
+  }
 
-	byte inputPin[4];
+  display.clearDisplay();
+  display.setCursor(0, 40);
 
-	pincode.reqPin(inputPin);
-	
-	for (int i=0; i<4; i++) {
-		if(inputPin[i] != pin[i]) {
-			ok = false;
-			break;
-		}
-	}
+  if (ok) {
+    display.print("OK");
+  } else {
+    display.print("NO");
+  }
+  display.display();
 
-	display.clearDisplay();
-	display.setCursor(0, 40);
-
-	if (ok) {
-		display.print("OK");
-	} else {
-		display.print("NO");
-	}
-	display.display();
-
-	delay(1000);
+  delay(1000);
 }
