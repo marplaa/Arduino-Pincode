241b8eeca1a15a8a55a84c03c097a3a93ff9b713
marplaa <marplaa@users.noreply.github.com>
2018-01-24 17:15:10 +0100

    Update Pincode.ino

--- examples/Pincode/Pincode.ino
+++ examples/Pincode/Pincode.ino
@@ -7,27 +7,31 @@
 #define OLED_RESET  15
 
 #define BUTTON      4
+#define ENCODER_1   20
+#define ENCODER_2   21
 
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
-Encoder myEnc(20, 21);
+Encoder myEnc(ENCODER_1, ENCODER_2);
 Pincode pincode(&display, &myEnc, BUTTON);
 
 byte pin[] = {1, 2, 3, 4};
 
 void setup() {
-
   display.begin(SSD1306_SWITCHCAPVCC);
-  pinMode(BUTTON, INPUT_PULLUP);           // set pin to input
-  pincode.setDialSpeed(20);
+  pinMode(BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
   byte inputPin[4];
   boolean ok = true;
+
+  // (Optional) A floating A0 as input for the RNG
   pincode.setRandomSeed(analogRead(A0));
 
+  // let the user enter the pin
   pincode.reqPin(inputPin);
 
+  // Check if pins match
   for (int i = 0; i < 4; i++) {
     if (inputPin[i] != pin[i]) {
       ok = false;
@@ -35,6 +39,7 @@ void loop() {
     }
   }
 
+  // Print "OK" if the pin is correct or "NO" otherwise
   display.clearDisplay();
   display.setCursor(0, 40);
 
